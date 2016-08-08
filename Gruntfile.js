b419c3f9f6aed0b1d77504b19cefcba655723ce8
fk-mbc <mail@florian-kittel.de>
2016-08-08 22:26:31 +0200

    removing grunt-contrib-sass and added grund-sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,9 +46,9 @@ module.exports = function (grunt) {
         files: ['test/spec/{,*/}*.js', '<%= yeoman.app %>/**/*.spec.js'],
         tasks: ['newer:jshint:test', 'newer:jscs:test', 'karma']
       },
-      compass: {
+      sass: {
         files: ['<%= yeoman.app %>/styles/**/*.{scss,sass}'],
-        tasks: ['compass:server', 'postcss:server']
+        tasks: ['sass:server', 'postcss:server']
       },
       gruntfile: {
         files: ['Gruntfile.js']
@@ -250,31 +250,30 @@ module.exports = function (grunt) {
       }
     },
     // Compiles Sass to CSS and generates necessary files if requested
-    compass: {
+    sass: {
       options: {
-        sassDir: '<%= yeoman.app %>/styles',
-        cssDir: '.tmp/styles',
-        generatedImagesDir: '.tmp/images/generated',
-        imagesDir: '<%= yeoman.app %>/images',
-        javascriptsDir: '<%= yeoman.app %>/scripts',
-        fontsDir: '<%= yeoman.app %>/styles/fonts',
-        importPath: './bower_components',
-        httpImagesPath: '/images',
-        httpGeneratedImagesPath: '/images/generated',
-        httpFontsPath: '/styles/fonts',
-        relativeAssets: false,
-        assetCacheBuster: false,
-        raw: 'Sass::Script::Number.precision = 10\n'
+        includePaths: [
+          'bower_components'
+        ],
+        sourceMap: true
       },
       dist: {
-        options: {
-          generatedImagesDir: '<%= yeoman.dist %>/images/generated'
-        }
+        files: [{
+            expand: true,
+            cwd: '<%= yeoman.app %>/styles',
+            src: ['*.scss'],
+            dest: '.tmp/styles',
+            ext: '.css'
+          }]
       },
       server: {
-        options: {
-          sourcemap: true
-        }
+        files: [{
+            expand: true,
+            cwd: '<%= yeoman.app %>/styles',
+            src: ['*.scss'],
+            dest: '.tmp/styles',
+            ext: '.css'
+          }]
       }
     },
     // Renames files for browser caching purposes
@@ -445,13 +444,15 @@ module.exports = function (grunt) {
     // Run some tasks in parallel to speed up the build process
     concurrent: {
       server: [
-        'compass:server'
+        'sass:server',
+        'copy:styles'
       ],
       test: [
-        'compass'
+        'copy:styles'
       ],
       dist: [
-        'compass:dist',
+        'sass',
+        'copy:styles',
         'imagemin',
         'svgmin'
       ]
